c4034507f0b147de9fd729deb21362aea053ebc2
Cynthia Zhong <cynthia.zz@berkeley.edu>
2020-04-29 13:46:50 -0700

    finished trimMST solver no testing

--- solver_trimMST.py
+++ solver_trimMST.py
@@ -8,38 +8,52 @@ import queue
 
 def solve(G):
 	tree = nx.minimum_spanning_tree(G)
-	mst_cost = average_pairwise_distance_fast(G, tree)
+	cost = average_pairwise_distance_fast(G, tree)
 
-	# max leaf heap containing (weight, leaf_node) tuples
-	leaf_heap = queue.PrioirtyQueue()
+	# max leaf heap containing (weight, leaf_edge) tuples
+	leaf_heap = queue.PriorityQueue()
+	# keep track of leaves in the heap
+	leaves_in_heap = set()
 
 	# keep pruning heaviest leaf from mst until the heaviest leaf becomes too light
 	# or the tree is no longer dominating
 	while True:
-		if not nx.is_dominating_set(G, tree):
-			break
-
+		# (node, degree) pairs
 		all_degrees = G.degree(G.nodes)
 		
 		# find all leaves
-		deg_to_nodes = defaultdict(list)
+		leaves = []
 		for node, deg in all_degrees:
-			deg_to_nodes[deg].append(node)
-		leaves = deg_to_nodes[1]
+			if deg == 1:
+				leaves.append(node)
 
 		# add leaves to PQ
 		for leaf in leaves:
 			leaf_neighbor = list(nx.neighbors(G, leaf))[0]
 			leaf_weight = G[leaf][leaf_neighbor]['weight']
-			leaf_heap.put((leaf_weight, (leaf, leaf_neighbor)))
+			# negate weight to use minPQ as maxPQ
+			entry = (-leaf_weight, (leaf, leaf_neighbor))
+			if entry not in leaves_in_heap:
+				leaf_heap.put(entry)
 
-		# prune heaviest leaf unless it's smaller than some threshold
-		heaviest_leaf = leaf_heap.get()
-		if heaviest_leaf[0] < mst_cost: # placeholder
+		
+		heaviest = leaf_heap.get()
+		edge = heaviest[1]
+		tree.remove_edge(edge[0], edge[1])
+		# if removing the edge makes the tree non-dominating, put it back
+		if not nx.is_dominating_set(G, tree):
+			tree.add_edge(edge[0], edge[1])
 			break
-		else:
-			leaf = heaviest_leaf[1]
-			leaf_neighbor = list(nx.neighbors(G, leaf))[0]
-			tree.remove_edge(leaf, leaf_neighbor)
+
+		# if removing the edge results in greater cost, put it back
+		temp_cost = average_pairwise_distance_fast(tree)
+		if temp_cost > cost:
+			tree.add_edge(edge[0], edge[1])
+			break
+
+		# the leaf is officially removed; update leaves set and cost
+		leaves_in_heap.remove(heaviest)
+		cost = temp_cost
 
 	return tree
+
